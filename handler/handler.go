b69f4a9a8564fb7aac4f8ad348c2197db4ac2dc7
Mohini Dhakad <99878435+Mohinidhakad@users.noreply.github.com>
2023-12-16 23:22:41 +0530

    Update handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -44,31 +44,29 @@ func (h handler) Create(ctx *gofr.Context) (interface{}, error) {
 }
 
 func (h handler) Update(ctx *gofr.Context) (interface{}, error) {
-	var emp models.Car
-	if err := ctx.Bind(&emp); err != nil {
-		ctx.Logger.Errorf("error in binding: %v", err)
-		return nil, errors.InvalidParam{Param: []string{"body"}}
-	}
+	carId := ctx.Param("id")
+	var car models.Car
 
-	resp, err := h.store.Create(ctx, emp)
+	car.ID, _ = strconv.Atoi(carId)
+	res, err := h.store.Update(ctx, car)
+
 	if err != nil {
-		return nil, err
+		return res, err
+
 	}
 
-	return resp, nil
+	return res, nil
 }
-
 func (h handler) Delete(ctx *gofr.Context) (interface{}, error) {
-	var emp models.Car
-	if err := ctx.Bind(&emp); err != nil {
-		ctx.Logger.Errorf("error in binding: %v", err)
-		return nil, errors.InvalidParam{Param: []string{"body"}}
-	}
+	carId := ctx.Param("id")
+	var car models.Car
 
-	resp, err := h.store.Create(ctx, emp)
+	car.ID, _ = strconv.Atoi(carId)
+
+	err := h.store.Delete(ctx, car.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	return resp, nil
+	return map[string]interface{}{"message": "Deletion successful", "id": carId}, nil
 }
